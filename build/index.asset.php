a5ad2ed1f8de2e7d88d53cc92cde2b4de878d972
Sriniva Billa <srinivasbilla12@gmail.com>
2025-09-02 11:37:44 -0400

    Update Js  search files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '2dade8faf070b468b68f');
+<?php return array('dependencies' => array('jquery'), 'version' => 'bca6a6de183dbb9db35c');
